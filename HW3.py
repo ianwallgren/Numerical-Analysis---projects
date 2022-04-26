a09fd4e08ffea7c8ca5c57c9f9846144c44d9e95
Tiancheng <102963139+just-miku@users.noreply.github.com>
2022-04-26 14:27:25 +0200

    Update HW3.py

--- HW3.py
+++ HW3.py
@@ -3,26 +3,87 @@ from numpy.linalg import solve
 import matplotlib.pyplot as plt
 # from scipy import *
 
+###### TASK 1 ######
 
 ##  Low-effort approach using polyfit
-x = [-1.9, -3.7, -5.77, 2.53, 4.32]
-y = [109.26, 92.4, 115.33, 107.77, 61.14]
-PC = polyfit(x,y,3)
+x = [-5, -4, -3, -2, -1]
+temp = [-1.9, -3.7, -5.77, 2.53, 4.32]
+energy = [109.26, 92.4, 115.33, 107.77, 61.14]
+PC_energy = polyfit(x, energy, 4)
+PC_temp = polyfit(x, temp, 4)
 
-my_poly = lambda x: PC[0]*x**3 + PC[1]*x**2 + PC[2]*x + PC[3]
-x_grid = linspace(-10,10,5000)
-y_grid = [my_poly(x) for x in x_grid]
+temp_poly = lambda x: PC_temp[0]*x**4 + PC_temp[1]*x**3 + PC_temp[2]*x**2 + PC_temp[3]*x + PC_temp[4]
+energy_poly = lambda x: PC_energy[0]*x**4 + PC_energy[1]*x**3 + PC_energy[2]*x**2 + PC_energy[3]*x + PC_energy[4]
+x_grid = linspace(-5,5,5000)
+temp_grid = [temp_poly(x) for x in x_grid]
+energy_grid = [energy_poly(x) for x in x_grid]
 
-plt.plot(x_grid,y_grid)
-plt.scatter(x,y)
-plt.title("Energy Usage interpolation vs Temperature")
+plt.plot(x_grid,temp_grid)
+plt.plot(x_grid,energy_grid)
+plt.scatter(x,temp)
+plt.scatter(x,energy)
+plt.title("Energy Usage interpolation vs days")
 plt.style.use("ggplot")
 plt.show()
+
+xp = 7
+print(xp, temp_poly(xp))
+
 
 
 
 ##  Vander Approach
-my_vander = vander(x,)
-yt = transpose(y)
+my_vander = vander(x,5)
+PC_temp_v = solve(my_vander,temp)
+PC_energy_v = solve(my_vander,energy)
 
-my_coeffs = solve(my_vander,yt)
+
+### Lagrange Approach
+
+#%%
+#Lagrange polynomial
+m = len(x)
+n = m-1 #degree of polynomial
+
+x = np.array([-5, -4, -3, -2, -1],float)
+y = np.array([-1.9, -3.7, -5.77, 2.53, 4.32],float)
+energy = np.array([109.26, 92.4, 115.33, 107.77, 61.14], float)
+
+xplt = np.linspace(x[0],x[-1])
+yplt = np.array([],float)
+
+for xp in xplt:
+    yp = 0
+    for xi,yi in zip(x,y):
+        yp += yi * np.prod((xp- x[x != xi])/(xi - x[x != xi]))
+    yplt = np.append(yplt,yp)
+
+
+plt.plot(x,y,'ro')
+plt.plot(xplt,yplt,'g-')
+
+
+
+
+
+###### TASK 2 ######
+
+n = 15
+inter = np.linspace(-1,1,n)
+inter_ = sorted(np.random.uniform(-1,1,n))
+
+
+wn = 0
+plt_wn = []
+
+def error(x,interval):
+    wn = 1
+    for i in range(len(inter)):
+        wn += wn * np.prod(x-inter[i])
+        plt_wn.append(wn)
+
+    return pltwn
+
+l = error(0.111,inter)
+plt.plot(inter_,l)
+plt.show()
